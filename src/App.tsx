25365c14a400f9c1077c963e5980eb810ea2b292
Jaewon Park <jaewonparkk73@gmail.com>
2026-05-20 11:34:16 -0400

    Add a navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import FeedPage from "./pages/FeedPage";
 import ClubsPage from "./pages/ClubsPage";
 import ClubDetailPage from "./pages/ClubDetailPage";
@@ -7,12 +7,35 @@ import "./App.css";
 
 function App() {
   return (
+    <>
+    <nav
+        style={{
+          padding: "14px 24px",
+          borderBottom: "1px solid #eee",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          background: "white",
+        }}
+      >
+        <Link to="/" style={{ fontWeight: 800, color: "#8b0028", textDecoration: "none" }}>
+          EagleCurrent
+        </Link>
+
+        <div style={{ display: "flex", gap: "18px" }}>
+          <Link to="/" style={{ textDecoration: "none", color: "#333" }}>Feed</Link>
+          <Link to="/clubs" style={{ textDecoration: "none", color: "#333" }}>Clubs</Link>
+          <Link to="/map" style={{ textDecoration: "none", color: "#333" }}>Map</Link>
+        </div>
+      </nav>
+
     <Routes>
       <Route path="/" element={<FeedPage />} />
       <Route path="/clubs" element={<ClubsPage />} />
       <Route path="/clubs/:orgId" element={<ClubDetailPage />} />
       <Route path="/map" element={<MapPage />} />
     </Routes>
+    </>
   );
 }
 
